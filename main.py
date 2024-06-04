51c9396807bfb234fbbbd1956bfca22102c59dfa
Z3R0GT <117790541+Z3R0GT@users.noreply.github.com>
2024-06-04 17:32:15 -0500

    test-1.1.5.3 - REFACTOR END (LST_LOAD)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from engine import *
 from engine.models.internal.tool.debug import _chk_window, print_debug
-from proyect_func import *
+#from proyect_func import *
 #from redirection import *
 from rec import *
 from time import sleep
@@ -61,7 +61,7 @@ def _procces_lst_info(*nm):
         _in_ += 10*sec
 
     if len(lst_pro[_in_-1]) >= 32:
-        nme = f"Mod's name: {lst_pro[_in_-1][:24]}..."
+        nme = f"Mod's name: {lst_pro[_in_-1][:16]}..."
     else:
         nme = f"Mod's name: {lst_pro[_in_-1]}"
 
@@ -90,48 +90,71 @@ def _procces_lst_info(*nm):
 
     menu.start_cast()
 
-def _procces_lst_refresh(*nm):
-    lst_pro:list; cur_len:int;menu:Page; nro_pg:int
-    lst_pro, cur_len, menu, nro_pg = nm[1][0], nm[1][1], nm[1][2], nm[1][3]
+def _is_in_range(is_new:bool,
+                 space_screen:int,
+                 cur_num:int, 
+                 rgn_in:range, 
+                 cur_list:dict) -> bool:
+    return not is_new and (cur_num-space_screen <= 0 or rgn_in[-1] >= cur_list["lst"][1]+space_screen)
 
-    menu.add_btn(menu.btns[0], False)
-    menu.add_btn(menu.btns[1], False)
+def __archive_lst(*nm):
+    menu:Page;cur_list:dict;is_foward:bool;cur_num:int;is_new:bool
+    ln_y:int; ln_x:int;num:int;lim_x:int
 
-    #ERASER
-    for nme in range(10):
-        menu.create_text(" "*(menu.meta["panel"][1]["transform"][0]-2), "CUSTOM", (1,nme+5))
-    #NME CASE
-    for in_ in range(10):
-        nme = in_ + len(lst_pro) - cur_len
-        if nme >= len(lst_pro):
-            break
-
-        if len(lst_pro[nme]) < menu.meta["panel"][1]["transform"][0]-2:
-            menu.create_text(lst_pro[nme], "CUSTOM", (1,in_+5)) 
-        else:
-            menu.create_text(lst_pro[nme][:20]+"...", "CUSTOM", (1,in_+5))
-
-    chk:bool=False
-    #RETURN CASE
-    if cur_len >= len(lst_pro):
-        menu.btns[0].caster((""), lst_pro, len(lst_pro), menu, 0)
-        chk = True
-        menu.del_btn(1, False)
-    else:
-        menu.btns[0].caster((""), lst_pro, cur_len+10, menu, nro_pg-1) 
+    menu, cur_list, is_foward, cur_num, is_new, ln_y, ln_x, num, lim_x = nm[1]
     
-    #NEXT CASE
-    if cur_len-10 <= 0:
-        menu.btns[1].caster((""), lst_pro, cur_len, menu, nro_pg)
-        menu.del_btn(2, False)
-        if chk:
-            menu.btns[3].var = (menu.btns[3].var[0], 0, menu.btns[3].var[2])
-        else:
-            menu.btns[3].var = (menu.btns[3].var[0], nro_pg+1, menu.btns[3].var[2])
+    SPACE_SCREEN  :int = 10
+    SPACE_LIMIT_X :int = lim_x
 
+    #RANGE TO UPDATE THE LIST
+    if is_foward:
+        rgn_in = range(cur_num-SPACE_SCREEN, cur_num)
     else:
-        menu.btns[1].caster((""), lst_pro, cur_len-10, menu, nro_pg+1)
-        menu.btns[3].var = (menu.btns[3].var[0], nro_pg+1, menu.btns[3].var[2])
+        rgn_in = range(cur_num, cur_num+SPACE_SCREEN)
+    
+    if not _is_in_range(is_new, SPACE_SCREEN, cur_num, rgn_in, cur_list):
+
+        for nme in range(SPACE_SCREEN): #ERASER SECTION
+            menu.create_text(" "*(menu.meta["panel"][1]["transform"][0]-2), "CUSTOM", (ln_x, nme+ln_y))
+    
+        
+        for in_ in rgn_in: #ADD TEXT
+            #TRIGGER WHEN THE "MAX" CASE COME
+            if in_ >= cur_list["lst"][1]:
+                ch = True 
+                break
+
+            if len(cur_list["lst"][0][in_]) < menu.meta["panel"][1]["transform"][0]-SPACE_LIMIT_X:
+                menu.create_text(f"{in_%SPACE_SCREEN+num+1}) "+cur_list["lst"][0][in_], "CUSTOM", (ln_x, (in_%SPACE_SCREEN)+ln_y))
+            else:
+                menu.create_text(f"{in_%SPACE_SCREEN+num+1}) "+cur_list["lst"][0][in_][:16]+"...", "CUSTOM", (ln_x, (in_%SPACE_SCREEN)+ln_y))
+
+            ch = False
+
+        if not is_new:
+            if is_foward:
+                if not menu.btns[0].var[3] >= cur_num+SPACE_SCREEN:
+                    menu.btns[1].caster((""), menu, cur_list, False, menu.btns[1].var[3]-SPACE_SCREEN, False, 5, 1, 0, 5)
+                    menu.add_btn(menu.btns[0], False)
+
+                    menu.btns[0].caster((""), menu, cur_list, True, menu.btns[0].var[3]+SPACE_SCREEN, False, 5, 1, 0, 5)
+                    menu.add_btn(menu.btns[1], False)
+
+                if ch:
+                    menu.del_btn(1, False)
+            elif not menu.btns[1].var[3] <= cur_num-SPACE_SCREEN:
+                menu.del_btn(2, False)
+            else:
+                menu.btns[0].caster((""), menu, cur_list, False, menu.btns[0].var[3]-SPACE_SCREEN, False, 5, 1, 0, 5)
+                menu.add_btn(menu.btns[1], False)
+
+                menu.btns[1].caster((""), menu, cur_list, True, menu.btns[1].var[3]+SPACE_SCREEN, False, 5, 1, 0, 5)
+                menu.add_btn(menu.btns[0], False)
+        else:
+            # THIS EXECUTE JUST ON THE START
+            menu.btns[0].caster((""), menu, cur_list, True, menu.btns[0].var[3]+SPACE_SCREEN, False, 5, 1, 0, 5)
+            menu.add_btn(menu.btns[0], False)
+            return
 
     menu.start_cast()
 
@@ -170,92 +193,98 @@ def proyect_new(*nm):
 
     menu.start_cast()
 
-def _proyect_list(*nm):
+def proyect_list(*nm):
     info = check_proyects()
     lst_pro = [i for i in info]
-    if not len(lst_pro) == 0:
-        menu = Page(X=size[0], Y=size[1]+4, CHR="#")
+    info_arch = {"lst":[lst_pro, len(lst_pro)]}
 
-        menu.create_text("Proyect list menu", "CUSTOM", (3, 3))
-        menu.create_text(f"version {ver}", "CUSTOM", (menu.vec[0]-len(ver)-12, 1))
-
-        menu.add_panel(0, 4, 27, 12, 0)
-        menu.add_panel(60, 3, 32, 6, 0)
-
-
-        menu.add_panel(60, 11, 32, 5, 0)
-        if len(lst_pro[0]) >= 32:
-            nme = f"Mod's name: {lst_pro[0][:24]}..."
-        else:
-            nme = f"Mod's name: {lst_pro[0]}"
-
-        ref = info[lst_pro[0]]
-        if not ref == "Error!":
-            inf = []
-            for _in in range(2):
-                if len(ref[_in]) >= 25:
-                    inf.append(ref[_in][:20]+"...")
-                else:
-                    inf.append(ref[_in])
-
-            aut = inf[0]
-            ver_ = inf[1]
-            lnk = ref[2]
-        else:
-            aut = "meta not found"
-            ver_ = "meta not found"
-            lnk = web
-
-        menu.create_text(nme, "CUSTOM", (62, 12))
-        menu.create_text(f"Autor: {aut}", "CUSTOM", (62, 13))
-        menu.create_text(f"Version: {ver_}", "CUSTOM", (62, 14))
-        
-        #NEED FIX
-        c=-1
-        for nme in range(10):
-            c+=1
-            if c == len(lst_pro):
-                break
-
-            if len(lst_pro[nme]) < menu.meta["panel"][1]["transform"][0]-2:
-                menu.create_text(lst_pro[nme], "CUSTOM", (1,nme+5)) 
-            else:
-                menu.create_text(lst_pro[nme][:20]+"...", "CUSTOM", (1,nme+5))
-
-        btn1 = Button(X=1, Y=17, TEXT="Return", ACTION=_procces_lst_refresh, DEFAULT="CUSTOM") 
-
-        btn2 = Button(X=15, Y=17, TEXT="Next", ACTION=_procces_lst_refresh, DEFAULT="CUSTOM")
-        
-        nro_pg = 0
-
-        btn1.caster((""), lst_pro, len(lst_pro), menu, nro_pg)
-        menu.add_btn(btn1)
-        menu.del_btn(1, False)
-
-        if len(lst_pro) >= 10:
-            btn2.caster((""), lst_pro, len(lst_pro)-10, menu, nro_pg)
-            menu.add_btn(btn2)
-
-        btn = Button(X=62, Y=15, TEXT="Autor site/download", ACTION=lnk, DEFAULT="LINK")
-        menu.add_btn(btn)
-
-        btn = Button(X=31, Y=17, TEXT="Archive to load",ACTION=_procces_lst_info, DEFAULT="CUSTOM")
-        btn.caster(("Enter proyect's number"), menu, nro_pg, info)
-        menu.add_btn(btn)
-
-        btn = Button(X=70, Y=17, TEXT="Load", ACTION=proyect_lst_pro, DEFAULT="CUSTOM")
-        btn.caster((""), info, lst_pro, 0)
-        menu.add_btn(btn)
-
-        btn = Button(X=84, Y=17, TEXT="Back", DEFAULT="BACK")
-        menu.add_btn(btn)
-
-        menu.start_cast()
-    else:
-        _chk_window()
-        print_debug(F"{ver}; DON'T PROYECT FOUND YET; {ver}")
+    #SECURITY CHECK
+    if len(lst_pro) == 0:
+        erase_screen()
+        print_debug(f"{ver} YOU DON'T PROYECTS CREATED YET {ver}")
+        print_debug(f"{ver} REDIRECTING TO CREATE A NEW... {ver}")
         sleep(5)
-        CUR[0][0].start_cast()
+        #proyect_new()
+
+    menu = Page(X=size[0], Y=size[1]+4, CHR="#", NMO="Proyect's page")  
+
+    menu.create_text("Proyect's page", "CUSTOM", (3,3))
+    menu.create_text(f"version {ver}", "CUSTOM", (menu.vec[0]-len(ver)-12, 1))
+
+    menu.add_panel(0, 4, 27, 12, 0)  #SELECT SECTION
+    menu.add_panel(60, 3, 32, 6, 0)  #TUTORIAL ""
+
+    menu.add_panel(60, 11, 32, 5, 0) #META ""
+
+    #FIRST META'S ARCHIVE LOADED
+    ref = info[lst_pro[0]]
+    if len(lst_pro[0]) >= 32:
+        nme = f"Mod's name: {lst_pro[0][:24]}..."
+    else:
+        nme = f"Mod's name: {lst_pro[0]}"
+    
+    if not ref == "Error!":
+        inf = []
+        for _in in range(2):
+            if len(ref[_in]) >= 25:
+                inf.append(ref[_in][:20]+"...")
+            else:
+                inf.append(ref[_in])
+        
+        autor = inf[0]
+        ver_mod = inf[1]
+        lnk = ref[2]
+        del inf, _in
+    else:
+        autor = "meta not found"
+        ver_mod = "meta not found"
+        lnk = web
+    del ref
+
+    menu.create_text(nme, "CUSTOM", (62, 12))
+    menu.create_text(f"Autor: {autor}", "CUSTOM", (62, 13))
+    menu.create_text(f"Version: {ver_mod}", "CUSTOM", (62, 14))
+    del autor, ver_mod
+
+    #OPTIONS'S SECTION
+    for nme in range(10):
+        if nme == len(lst_pro):
+            break
+
+        if len(lst_pro[nme]) < menu.meta["panel"][1]["transform"][0]-5:
+            menu.create_text(f"{nme+1}) "+lst_pro[nme], "CUSTOM", (1,nme+5)) 
+        else:
+            menu.create_text(f"{nme+1}) "+lst_pro[nme][:16]+"...", "CUSTOM", (1,nme+5))
+    del nme
+
+    btn = Button(1, 17, "Next", __archive_lst, DEFAULT="CUSTOM")
+    btn.caster((""), menu, info_arch, True, 10, False, 5, 1, 0, 5)
+    menu.add_btn(btn)
+    menu.del_btn(1, False)
+
+    __archive_lst((""), [menu, info_arch, False, 0, True, 5, 1, 0, 5])
+
+    btn = Button(15, 17, "Back", __archive_lst, DEFAULT="CUSTOM")
+    btn.caster((""), menu, info_arch, False, 10, False, 5, 1, 0, 5)
+    menu.add_btn(btn)
+    menu.del_btn(2, False)
+
+    btn = Button(X=62, Y=15, TEXT="Autor site/download", ACTION=lnk, DEFAULT="LINK")
+    menu.add_btn(btn)
+
+    btn = Button(X=31, Y=17, TEXT="Archive to load",ACTION=_procces_lst_info, DEFAULT="CUSTOM")
+    btn.caster(("Enter proyect's number"), menu, 0, info)
+    menu.add_btn(btn)
+
+    btn = Button(X=70, Y=17, TEXT="Load", ACTION=proyect_lst_pro, DEFAULT="CUSTOM")
+    btn.caster((""), info, lst_pro, 0)
+    menu.add_btn(btn)
+
+    btn = Button(X=84, Y=17, TEXT="Back menu", DEFAULT="BACK")
+    menu.add_btn(btn)
+    del btn
+
+    menu.start_cast()
 
 page = Page(X=size[0], Y=size[1], CHR="#")
 page.create_text("Roses In The Flame's mod engine", "CUSTOM", (3, 3))
